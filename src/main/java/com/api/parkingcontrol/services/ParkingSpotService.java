f0357eeef908d1765260ac43d7d7b28dc915b735
Maycon Aquino <mayyconaquino@gmail.com>
2022-02-13 18:54:49 -0300

    Implementações dos metodos de pesquisa e delete

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,5 +1,9 @@
 package com.api.parkingcontrol.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
@@ -9,18 +13,42 @@ import com.api.parkingcontrol.repositores.ParkingSpotRepository;
 
 @Service
 public class ParkingSpotService {
-	
-	 final ParkingSpotRepository parkingSpotRepository;
+
+	final ParkingSpotRepository parkingSpotRepository;
 
 	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
 		this.parkingSpotRepository = parkingSpotRepository;
 	}
 
 	@Transactional
-	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {		
+	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
 		return parkingSpotRepository.save(parkingSpotModel);
 	}
-	 
-	 
+
+	public boolean existsByLicensePlateCar(String licensePlateCar) {
+		return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+	}
+
+	public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+		return existsByParkingSpotNumber(parkingSpotNumber);
+	}
+
+	public boolean existsByApartmentAndBlock(String apartment, String block) {
+		return existsByApartmentAndBlock(apartment, block);
+	}
+
+	public List<ParkingSpotModel> findAll() {
+		return parkingSpotRepository.findAll();
+	}
+
+	public Optional<ParkingSpotModel> findById(UUID id) {
+		return parkingSpotRepository.findById(id);
+	}
+	
+	@Transactional
+	public void delete(ParkingSpotModel parkingSpotModel) {
+		parkingSpotRepository.delete(parkingSpotModel);
+
+	}
 
 }
